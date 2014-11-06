0bcb8af781af12395e157d920412fcf972f94014
sanoelle <snhodgson@yahoo.ca>
2014-11-06 15:59:02 -0500

    Adjusted position of nav & gave it bg colour

---
+++ js/nav-stick.js
@@ -0,0 +1,12 @@
+//NAV STICK
+var  n = $(".nav");
+    ns = "nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
