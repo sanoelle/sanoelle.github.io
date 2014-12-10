eac840f1426e4f5e787778886ac911290ffec6ce
sanoelle <snhodgson@yahoo.ca>
2014-12-09 23:22:46 -0500

    Added content

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var $win = $(window);
 var $mainBannerSection = $('.main-banner-section');
 var $extraordinarySection = $('.extraordinary-section');
 var $extraordinary = $('.extraordinary');
+var $topBtn = $('.top-btn');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -12,11 +13,21 @@ $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 	$extraordinarySection.css('background-position', 'center ' + scrollPos / 1 + 'px');
 });
-	
+
 	
 $extraordinarySection.waypoint(function (){
 	$extraordinary.addClass('js-extraordinary-fade');
 }, { offset: '50%' });
+
+$topBtn.localScroll();
+
+$('.about-section').waypoint(function (direction) {
+	if(direction=="down"){
+		$topBtn.attr('data-state', 'visible');
+	} else {
+		$topBtn.attr('data-state', 'hidden');
+	}
+});
 
 	
 //$(document).ready(function() {
